4f7f11e5f081dd355d7770f293da3033509c7a3c
grzegorz-kolaty <grzegorzkolaty@gmail.com>
2025-02-14 11:54:22 +0100

    i thil cleaned up version to use

--- e-vet-szczecin/src/app/app.config.ts
+++ e-vet-szczecin/src/app/app.config.ts
@@ -1,7 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp } from 'firebase/app';
 
 import { routes } from './app.routes';
+import {environment} from '../environments/environments';
+const app = initializeApp(environment.firebase);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
---
+++ e-vet-szczecin/src/environments/environments.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyCOpC8X9SUxGIyGzMCifCmzdEV9scOayEw",
+    authDomain: "e-vet-szczecin.firebaseapp.com",
+    projectId: "e-vet-szczecin",
+    storageBucket: "e-vet-szczecin.firebasestorage.app",
+    messagingSenderId: "36520869364",
+    appId: "1:36520869364:web:5fe370b10529f6a5340da1",
+    measurementId: "G-J290P2C8WY"
+  }
+}
